8977ab0ac6d04e0952b22a3aa2ba0665dd84168d
Sergo-Dalai-Lama <sergo_bazuka@mail.ru>
2025-08-03 02:34:32 +0300

    Добавил исправление - остановку по команде для сервера

--- my_random.py
+++ my_random.py
@@ -1,30 +1,25 @@
+# my_random.py
 import random
 import time
 import threading
-import sys
+import signal
+
+stop_flag = threading.Event()
 
 def generate_numbers():
     while not stop_flag.is_set():
         num = random.randint(1, 100)
-        with open("rnd.txt", "w") as f:  # 'w' - перезапись файла
+        with open("rnd.txt", "w") as f:
             f.write(f"{num}\n")
         time.sleep(10)
 
-def wait_for_exit():
-    input("Нажмите Enter для остановки...\n")
+def handle_signal(signum, frame):
     stop_flag.set()
 
 if __name__ == "__main__":
-    stop_flag = threading.Event()
+    # Регистрируем обработчик сигнала
+    signal.signal(signal.SIGTERM, handle_signal)
     
-    # Запускаем генерацию чисел в отдельном потоке
     generator_thread = threading.Thread(target=generate_numbers)
     generator_thread.start()
-    
-    # Ожидаем нажатия Enter в основном потоке
-    wait_for_exit()
-    
-    # Дожидаемся завершения потока с генерацией
     generator_thread.join()
-    
-    print("Программа остановлена")
