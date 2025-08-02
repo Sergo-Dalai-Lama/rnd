b9d1dd33694a132496c938549711c0d1246ee5d2
Sergo-Dalai-Lama <sergo_bazuka@mail.ru>
2025-08-03 02:12:49 +0300

    Добавил возможность останавливать программу

--- my_random.py
+++ my_random.py
@@ -1,8 +1,30 @@
 import random
 import time
+import threading
+import sys
 
-while True:
-    with open("rnd.txt", "a") as f:
+def generate_numbers():
+    while not stop_flag.is_set():
         num = random.randint(1, 100)
-        f.write(f"{num}\n")
-    time.sleep(10)
+        with open("rnd.txt", "w") as f:  # 'w' - перезапись файла
+            f.write(f"{num}\n")
+        time.sleep(10)
+
+def wait_for_exit():
+    input("Нажмите Enter для остановки...\n")
+    stop_flag.set()
+
+if __name__ == "__main__":
+    stop_flag = threading.Event()
+    
+    # Запускаем генерацию чисел в отдельном потоке
+    generator_thread = threading.Thread(target=generate_numbers)
+    generator_thread.start()
+    
+    # Ожидаем нажатия Enter в основном потоке
+    wait_for_exit()
+    
+    # Дожидаемся завершения потока с генерацией
+    generator_thread.join()
+    
+    print("Программа остановлена")
